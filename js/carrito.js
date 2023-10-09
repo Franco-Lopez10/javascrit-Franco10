848316c012fb768f0f8f26b628d849543f801772
Franco10 <frannchu_lopez@hotmail.com>
2023-10-08 22:37:39 -0300

    comentario

--- js/carrito.js
+++ js/carrito.js
@@ -1,13 +1,13 @@
 const pintarCarrito = () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "flex";
-
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header"
     modalHeader.innerHTML = `
     <h1 class="modal-header-title">Carrito.</h1>
     `;
     modalContainer.append(modalHeader);
+
     const modalbutton = document.createElement("h1");
     modalbutton.innerText = "x";
     modalbutton.className = "modal-header-button";
@@ -18,19 +18,19 @@ const pintarCarrito = () => {
 
     modalHeader.append(modalbutton);
 
-
     carrito.forEach((product) => {
         let carritoContent = document.createElement("div")
         carritoContent.className = "modal-content"
         carritoContent.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p>${product.precio} $</p>
-    <span class ="restar"> - </span>
-    <p>cantidad ${product.cantidad}</p>
-    <span class ="sumar"> + </span>
-    <p>Total: ${product.cantidad * product.precio}</p>
-    `;
+            <img src="${product.img}">
+            <h3>${product.nombre}</h3>
+            <p>${product.precio} $</p>
+            <span class ="restar"> - </span>
+            <p>cantidad ${product.cantidad}</p>
+            <span class ="sumar"> + </span>
+            <p>Total: ${product.cantidad * product.precio}</p>
+            <span class="delete-product"> ❌ </span> 
+            `;
 
         modalContainer.append(carritoContent);
 
@@ -45,33 +45,63 @@ const pintarCarrito = () => {
         });
 
         let sumar = carritoContent.querySelector(".sumar")
-
         sumar.addEventListener("click", () => {
             product.cantidad++;
             saveLocal();
             pintarCarrito();
         });
 
-        let eliminar = document.createElement("span");
-        eliminar.innerText = "❌";
-        eliminar.className = "delete-product";
-        carritoContent.append(eliminar);
+        let eliminar = carritoContent.querySelector(".delete-product");
+        
+        eliminar.addEventListener("click", () => {
+            eliminarProducto(product.id);
+            pintarCarrito();  // Actualizar la vista del carrito despues de elimiar
+            });
+        });
 
-        eliminar.addEventListener("click", eliminarProducto);
-    });
+ const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
 
-    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
-
-    const totalBuying = document.createElement("div")
-    totalBuying.className = "total-content"
+    const totalBuying = document.createElement("div");
+    totalBuying.className = "total-content";
     totalBuying.innerHTML = `total a pagar: ${total} $`;
-    modalContainer.append(totalBuying);
+
+// Crear el botón de Pagar
+const pagarButton = document.createElement("button");
+pagarButton.className = "pagar-button";
+pagarButton.textContent = "Comprar";
+
+// Agregar evento de click al botón
+pagarButton.addEventListener("click", () => {
+    // Aqui puedes agregar la lógica para procesar el pago
+    // Vaciar el carrito al procesar el pago
+    carrito.length = 0;
+    // Limpiar el contenido del modalContainer y mostrar un mensaje
+    modalContainer.innerHTML = "";
+    const mensajePago = document.createElement("p");
+    modalContainer.appendChild(mensajePago);
+    Swal.fire({
+        position:'center',
+        icon: 'success',
+        title: '¡Gracias por la compra.Disfrute su vino!',
+        showConfirmButton: false,
+        timer: 2000
+    })
+    // Actualizar el contador del carritp y ocultarlo
+    cantidadCarrito.style.display = "none";
+    localStorage.setItem("carritoLength", JSON.stringify(0));
+});
+
+// Agregar el contenido al modalContainer
+modalContainer.appendChild(totalBuying);
+modalContainer.appendChild(pagarButton);
 };
 
 verCarrito.addEventListener("click", pintarCarrito);
 
-const eliminarProducto = () => {
-    const foundId = carrito.find((element) => element.id);
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+
+    console.log(foundId);
 
     carrito = carrito.filter((carritoId) => {
         return carritoId !== foundId;
@@ -80,11 +110,10 @@ const eliminarProducto = () => {
     carritoCounter();
     saveLocal();
     pintarCarrito();
-
 };
 
 const carritoCounter = () => {
-    cantidadCarrito.style.display = "block"
+    cantidadCarrito.style.display = "block";
 
     const carritoLength = carrito.length;
 
